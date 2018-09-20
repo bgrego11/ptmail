2aa322873359872c9020d4e7b62022188474293c
bgrego11 <natureboy4271@gmail.com>
2018-09-20 17:37:57 -0400

    added catch

--- mailer/views.py
+++ mailer/views.py
@@ -7,16 +7,19 @@ import requests
 # Create your views here.
 @csrf_exempt
 def index(request):
-    data = json.loads(request.body)
-    fname = data['fname']
-    lname = data['lname']
-    email = data['email']
-    msg = data['msg']
-    requests.post(
-        "https://api.mailgun.net/v3/piranhatechnologies.com/messages",
-        auth=("api", "key-774f995362ec727cc4819fa705c1313d"),
-        data=({"from": "P Tech <mailgun@piranhatechnologies.com>",
-              "to": [ "benja.gregory2@gmail.com","snydzllc@gmail.com"],
-              "subject": "Hello",
-              "text": fname + ' ' +lname + ' sent a message. They say '+ msg}))
-    return HttpResponse("email sent")
+    try:
+        data = json.loads(request.body)
+        fname = data['fname']
+        lname = data['lname']
+        email = data['email']
+        msg = data['msg']
+        requests.post(
+            "https://api.mailgun.net/v3/piranhatechnologies.com/messages",
+            auth=("api", "key-774f995362ec727cc4819fa705c1313d"),
+            data=({"from": "P Tech <mailgun@piranhatechnologies.com>",
+                "to": [ "benja.gregory2@gmail.com","snydzllc@gmail.com"],
+                "subject": "Hello",
+                "text": fname + ' ' +lname + ' sent a message. They say '+ msg + 'reply at '+email}))
+        return HttpResponse("email sent")
+    except:
+        return HttpResponse("send failed")
